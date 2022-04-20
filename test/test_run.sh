f5aa9ce72f156f474df654e64662063aab0d278d
kaiiam <kblumberg@email.arizona.edu>
2022-04-20 15:29:21 +0200

    update

--- test/test_run.sh
+++ test/test_run.sh
@@ -12,8 +12,8 @@
 ### REQUIRED. Set the memory required for this job.
 #SBATCH --mem-per-cpu=5gb
 ### REQUIRED. Specify the time required for this job, hhh:mm:ss
-#SBATCH --time=4:00:00
+#SBATCH --time=1:00:00
 
 source activate humann_kai
 
-humann -i test.fastq.gz -o test_results
+humann -i test.fastq.gz -o slurm1_results
