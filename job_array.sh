aab9ebf37e9a5ca5abe18b4569c6c3e159a83a19
kaiiam <kblumberg@email.arizona.edu>
2022-04-21 15:48:10 +0200

    Rename

---
+++ job_array.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=human_test
+#SBATCH --account=bhurwitz
+#SBATCH --partition=standard
+#SBATCH --time=1:00:00
+#SBATCH --ntasks=94
+#SBATCH --nodes=1
+#SBATCH --mem-per-cpu=5gb
+
+# load environment
+source activate humann_kai
+
+# echo for log
+echo "job started"; hostname; date
+
+# Get sample ID
+export SMPLE=`head -n +${SLURM_ARRAY_TASK_ID} config/list.txt | tail -n 1`
+echo $SMPLE
+
+#run humann
+#humann --input input/$SMPLE --output results
+
+# Test run humann
+humann --input test/$SMPLE --output test_results
