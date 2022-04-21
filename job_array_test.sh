dca6edf89cdb6b21280148e857f23eb6a0cce83b
kaiiam <kblumberg@email.arizona.edu>
2022-04-21 15:06:29 +0200

    Add job array test script

---
+++ job_array_test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=human_test
+#SBATCH --account=bhurwitz
+#SBATCH --output=errout/outputr%j.txt
+#SBATCH --error=errout/errors_%j.txt
+#SBATCH --partition=standard
+#SBATCH --time=1:00:00
+#SBATCH --ntasks=20
+#SBATCH --nodes=1
+#SBATCH --mem-per-cpu=5gb
+
+# load environment
+source activate humann
+
+# echo for log
+echo "job started"; hostname; date
+
+# Get sample ID
+export SMPLE=`head -n +${SLURM_ARRAY_TASK_ID} config/list.txt | tail -n 1`
+echo $SMPLE
+
+# #run humann
+# humann --input input/$SMPLE --output results
