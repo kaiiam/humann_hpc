5f7d7616bd49b70e79f77006e85b1730bde73f0d
kaiiam <kblumberg@email.arizona.edu>
2022-05-17 10:49:42 +0200

    Testing fixes

--- test/prep_rarefaction_curve_data.sh
+++ test/prep_rarefaction_curve_data.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 #INPUT_DIR="../../../heidi/karnes_metagenomes/"
-$SMPLE1="../../../heidi/karnes_metagenomes/Karnes-10-1_UA-NGsp-fastq_Karnes-C11-U035_Karnes-C11_S285_R1_001.fastq.gz"
+#$SMPLE1="../../../heidi/karnes_metagenomes/Karnes-10-1_UA-NGsp-fastq_Karnes-C11-U035_Karnes-C11_S285_R1_001.fastq.gz"
 
-gunzip -c $SMPLE1 | head -n 10
+#gunzip -c $SMPLE1 | head -n 10
+
+gunzip -c  ../../../heidi/karnes_metagenomes/Karnes-10-1_UA-NGsp-fastq_Karnes-C11-U035_Karnes-C11_S285_R1_001.fastq.gz | head -n 10
