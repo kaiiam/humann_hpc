d18c84471b146745abe5ccb6e5acd73bb08b2e0f
kaiiam <kblumberg@email.arizona.edu>
2022-06-14 15:27:29 +0200

    Add more rarefaction test cases

--- test/prep_rarefaction_curve_data.sh
+++ test/prep_rarefaction_curve_data.sh
@@ -2,26 +2,35 @@
 #SBATCH --job-name=prep_rarefaction_data
 #SBATCH --account=bhurwitz
 #SBATCH --partition=standard
-#SBATCH --time=72:00:00
+#SBATCH --time=24:00:00
 #SBATCH --nodes=1
 
-# 5 Million
-gunzip -c  ../../../heidi/karnes_metagenomes/Karnes-10-1_UA-NGsp-fastq_Karnes-C11-U035_Karnes-C11_S285_R1_001.fastq.gz | head -n 20000000 | gzip > sample1_5M.fastq.gz
+# # 5 Million
+# gunzip -c  ../../../heidi/karnes_metagenomes/Karnes-10-1_UA-NGsp-fastq_Karnes-C11-U035_Karnes-C11_S285_R1_001.fastq.gz | head -n 20000000 | gzip > sample1_5M.fastq.gz
+#
+# # 10 Million
+# gunzip -c  ../../../heidi/karnes_metagenomes/Karnes-10-1_UA-NGsp-fastq_Karnes-C11-U035_Karnes-C11_S285_R1_001.fastq.gz | head -n 40000000 | gzip > sample1_10M.fastq.gz
+#
+# # 15 Million
+# gunzip -c  ../../../heidi/karnes_metagenomes/Karnes-10-1_UA-NGsp-fastq_Karnes-C11-U035_Karnes-C11_S285_R1_001.fastq.gz | head -n 60000000 | gzip > sample1_15M.fastq.gz
+#
+# # 20 Million
+# gunzip -c  ../../../heidi/karnes_metagenomes/Karnes-10-1_UA-NGsp-fastq_Karnes-C11-U035_Karnes-C11_S285_R1_001.fastq.gz | head -n 80000000 | gzip > sample1_20M.fastq.gz
+#
+# # 30 Million
+# gunzip -c  ../../../heidi/karnes_metagenomes/Karnes-10-1_UA-NGsp-fastq_Karnes-C11-U035_Karnes-C11_S285_R1_001.fastq.gz | head -n 120000000 | gzip > sample1_30M.fastq.gz
+#
+# # 40 Million
+# gunzip -c  ../../../heidi/karnes_metagenomes/Karnes-10-1_UA-NGsp-fastq_Karnes-C11-U035_Karnes-C11_S285_R1_001.fastq.gz | head -n 160000000 | gzip > sample1_40M.fastq.gz
+#
+# # 60 Million
+# gunzip -c  ../../../heidi/karnes_metagenomes/Karnes-10-1_UA-NGsp-fastq_Karnes-C11-U035_Karnes-C11_S285_R1_001.fastq.gz | head -n 240000000 | gzip > sample1_60M.fastq.gz
 
-# 10 Million
-gunzip -c  ../../../heidi/karnes_metagenomes/Karnes-10-1_UA-NGsp-fastq_Karnes-C11-U035_Karnes-C11_S285_R1_001.fastq.gz | head -n 40000000 | gzip > sample1_10M.fastq.gz
+# 80 Million
+gunzip -c  ../../../heidi/karnes_metagenomes/Karnes-10-1_UA-NGsp-fastq_Karnes-C11-U035_Karnes-C11_S285_R1_001.fastq.gz | head -n 320000000 | gzip > sample1_80M.fastq.gz
 
-# 15 Million
-gunzip -c  ../../../heidi/karnes_metagenomes/Karnes-10-1_UA-NGsp-fastq_Karnes-C11-U035_Karnes-C11_S285_R1_001.fastq.gz | head -n 60000000 | gzip > sample1_15M.fastq.gz
+# 100 Million
+gunzip -c  ../../../heidi/karnes_metagenomes/Karnes-10-1_UA-NGsp-fastq_Karnes-C11-U035_Karnes-C11_S285_R1_001.fastq.gz | head -n 400000000 | gzip > sample1_100M.fastq.gz
 
-# 20 Million
-gunzip -c  ../../../heidi/karnes_metagenomes/Karnes-10-1_UA-NGsp-fastq_Karnes-C11-U035_Karnes-C11_S285_R1_001.fastq.gz | head -n 80000000 | gzip > sample1_20M.fastq.gz
-
-# 30 Million
-gunzip -c  ../../../heidi/karnes_metagenomes/Karnes-10-1_UA-NGsp-fastq_Karnes-C11-U035_Karnes-C11_S285_R1_001.fastq.gz | head -n 120000000 | gzip > sample1_30M.fastq.gz
-
-# 40 Million
-gunzip -c  ../../../heidi/karnes_metagenomes/Karnes-10-1_UA-NGsp-fastq_Karnes-C11-U035_Karnes-C11_S285_R1_001.fastq.gz | head -n 160000000 | gzip > sample1_40M.fastq.gz
-
-# 60 Million
-gunzip -c  ../../../heidi/karnes_metagenomes/Karnes-10-1_UA-NGsp-fastq_Karnes-C11-U035_Karnes-C11_S285_R1_001.fastq.gz | head -n 240000000 | gzip > sample1_60M.fastq.gz
+# 140 Million
+gunzip -c  ../../../heidi/karnes_metagenomes/Karnes-10-1_UA-NGsp-fastq_Karnes-C11-U035_Karnes-C11_S285_R1_001.fastq.gz | head -n 560000000 | gzip > sample1_140M.fastq.gz
