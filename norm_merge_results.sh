c8ffc5ac501b357d261b41ecd67ec9eceac9f999
kaiiam <kblumberg@email.arizona.edu>
2022-07-07 14:02:49 -0400

    Add script to make normalized and merged outputs

---
+++ norm_merge_results.sh
@@ -0,0 +1,43 @@
+# load environment
+source activate humann_kai
+
+IN_DIR="/xdisk/bhurwitz/mig2020/rsgrps/bhurwitz/kai/humann_hpc/test_join"
+
+OUT_DIR="/xdisk/bhurwitz/mig2020/rsgrps/bhurwitz/kai/humann_hpc/test_join/cpm_norm_path"
+
+cd $IN_DIR
+mkdir $OUT_DIR
+
+# merge table and stratify
+
+mkdir result_tables
+humann_join_tables --input $OUT_DIR --output humann_pathabundance.tsv --file_name cpm_WGS
+humann_split_stratified_table --input humann_pathabundance.tsv --output result_tables
+mv humann_pathabundance.tsv result_tables
+
+
+OUT_DIR="/xdisk/bhurwitz/mig2020/rsgrps/bhurwitz/kai/humann_hpc/test_join/cpm_norm_KO"
+
+
+cd $IN_DIR
+mkdir $OUT_DIR
+
+# normalize gene abundance and group by KEGG
+for f in *_genefamilies.tsv
+do
+    Kf="${f%%.tsv}KEGG.tsv"
+    humann_regroup_table --input $f --groups uniref90_ko --output $Kf
+    humann_renorm_table --input $Kf --output cpm_$Kf --units cpm
+    mv cpm_$Kf $OUT_DIR
+done
+
+# merge table and stratify
+
+humann_join_tables --input $OUT_DIR --output humann_KOabundance.tsv --file_name cpm_WGS
+humann_split_stratified_table --input humann_KOabundance.tsv --output result_tables
+mv humann_KOabundance.tsv result_tables
+
+# all without norm
+humann_join_tables --input . --output humann_KOnonnorm.tsv --file_name _genefamiliesKEGG.tsv
+humann_split_stratified_table --input humann_KOnonnorm.tsv --output result_tables_nonnorm
+mv humann_KOnonnorm.tsv result_tables_nonnorm
